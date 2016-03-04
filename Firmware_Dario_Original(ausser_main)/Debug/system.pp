1a30b31f4ec463ef95baee02d219cd4d26bde170
coolt <katrin@coolt.ch>
2016-03-04 18:01:09 +0100

    Firmware Dario, Firmware Bicycle Computer: init
    Original, bis auf Main (bereits kommentiert)

---
+++ Firmware_Dario_Original(ausser_main)/Debug/system.pp
@@ -0,0 +1,61 @@
+# FIXED
+
+system.obj: ../system.c
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/aux_wuc.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdbool.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/yvals.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdarg.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/linkage.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/_lock.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdint.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_types.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_chip_def.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_memmap.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_aux_wuc.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/debug.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/rom.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/cpu.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_aon_wuc.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_ints.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_nvic.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_prcm.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/osc.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_ddi.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_ddi_0_osc.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/aon_wuc.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_aon_rtc.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/../driverLib/interrupt.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/ddi.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_aux_smph.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/vims.h
+system.obj: C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_vims.h
+
+../system.c: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/aux_wuc.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/_lock.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdint.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_types.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_chip_def.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_memmap.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_aux_wuc.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/debug.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/rom.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/cpu.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_aon_wuc.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_ints.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_nvic.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_prcm.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/osc.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_ddi.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_ddi_0_osc.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/aon_wuc.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_aon_rtc.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/../driverLib/interrupt.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/../driverLib/ddi.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_aux_smph.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/driverLib/vims.h: 
+C:/Users/katrin/workspace_v6_1/simplebroadcast_improved_recharge/cc26xxware_2_22_00_16101/inc/hw_vims.h: 
