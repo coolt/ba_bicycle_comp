e6669bf6c7d1ec50240af3f9c0362ee2347459f3
coolt <katrin@coolt.ch>
2016-03-11 18:03:56 +0100

    Grundversion bicycle computer funktioniert
    Sie entspricht Darios main. Sendet nur Pakete auf Knopfdruck.

---
+++ Firmware_bicycle_computer_V1/Debug/system.pp
@@ -0,0 +1,61 @@
+# FIXED
+
+system.obj: ../system.c
+system.obj: ../cc26xxware_2_22_00_16101/driverLib/aux_wuc.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdbool.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/yvals.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdarg.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/linkage.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/_lock.h
+system.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdint.h
+system.obj: C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_types.h
+system.obj: C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_chip_def.h
+system.obj: C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_memmap.h
+system.obj: C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_aux_wuc.h
+system.obj: ../cc26xxware_2_22_00_16101/driverLib/debug.h
+system.obj: ../cc26xxware_2_22_00_16101/driverLib/../driverLib/rom.h
+system.obj: ../cc26xxware_2_22_00_16101/driverLib/cpu.h
+system.obj: ../cc26xxware_2_22_00_16101/inc/hw_aon_wuc.h
+system.obj: ../cc26xxware_2_22_00_16101/inc/hw_ints.h
+system.obj: ../cc26xxware_2_22_00_16101/inc/hw_nvic.h
+system.obj: ../cc26xxware_2_22_00_16101/inc/hw_prcm.h
+system.obj: ../cc26xxware_2_22_00_16101/driverLib/osc.h
+system.obj: C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_ddi.h
+system.obj: C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_ddi_0_osc.h
+system.obj: ../cc26xxware_2_22_00_16101/driverLib/aon_wuc.h
+system.obj: C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_aon_rtc.h
+system.obj: ../cc26xxware_2_22_00_16101/driverLib/interrupt.h
+system.obj: ../cc26xxware_2_22_00_16101/driverLib/ddi.h
+system.obj: C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_aux_smph.h
+system.obj: ../cc26xxware_2_22_00_16101/driverLib/vims.h
+system.obj: C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_vims.h
+
+../system.c: 
+../cc26xxware_2_22_00_16101/driverLib/aux_wuc.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/_lock.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdint.h: 
+C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_types.h: 
+C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_chip_def.h: 
+C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_memmap.h: 
+C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_aux_wuc.h: 
+../cc26xxware_2_22_00_16101/driverLib/debug.h: 
+../cc26xxware_2_22_00_16101/driverLib/../driverLib/rom.h: 
+../cc26xxware_2_22_00_16101/driverLib/cpu.h: 
+../cc26xxware_2_22_00_16101/inc/hw_aon_wuc.h: 
+../cc26xxware_2_22_00_16101/inc/hw_ints.h: 
+../cc26xxware_2_22_00_16101/inc/hw_nvic.h: 
+../cc26xxware_2_22_00_16101/inc/hw_prcm.h: 
+../cc26xxware_2_22_00_16101/driverLib/osc.h: 
+C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_ddi.h: 
+C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_ddi_0_osc.h: 
+../cc26xxware_2_22_00_16101/driverLib/aon_wuc.h: 
+C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_aon_rtc.h: 
+../cc26xxware_2_22_00_16101/driverLib/interrupt.h: 
+../cc26xxware_2_22_00_16101/driverLib/ddi.h: 
+C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_aux_smph.h: 
+../cc26xxware_2_22_00_16101/driverLib/vims.h: 
+C:/Users/katrin/Documents/Dokumente/ZHAW/5Jahr/ba_bicycle_comp/Firmware_bicycle_computer/cc26xxware_2_22_00_16101/startup_files/../inc/hw_vims.h: 
