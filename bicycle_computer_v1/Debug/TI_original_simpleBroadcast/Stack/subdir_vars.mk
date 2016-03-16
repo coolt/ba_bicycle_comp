de5d0308d03b0d7a5da493c173a2f6dce9b15437
coolt <katrin@coolt.ch>
2016-03-16 10:22:59 +0100

    update

---
+++ bicycle_computer_v1/Debug/TI_original_simpleBroadcast/Stack/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../TI_original_simpleBroadcast/Stack/OSAL_ICallBle.c 
+
+OBJS += \
+./TI_original_simpleBroadcast/Stack/OSAL_ICallBle.obj 
+
+C_DEPS += \
+./TI_original_simpleBroadcast/Stack/OSAL_ICallBle.pp 
+
+C_DEPS__QUOTED += \
+"TI_original_simpleBroadcast\Stack\OSAL_ICallBle.pp" 
+
+OBJS__QUOTED += \
+"TI_original_simpleBroadcast\Stack\OSAL_ICallBle.obj" 
+
+C_SRCS__QUOTED += \
+"../TI_original_simpleBroadcast/Stack/OSAL_ICallBle.c" 
+
+
---
+++ bicycle_computer_v1/Debug/TI_original_simpleBroadcast/subdir_vars.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../TI_original_simpleBroadcast/main_TIexample.c \
+../TI_original_simpleBroadcast/simpleBLEBroadcaster_TIexample.c 
+
+OBJS += \
+./TI_original_simpleBroadcast/main_TIexample.obj \
+./TI_original_simpleBroadcast/simpleBLEBroadcaster_TIexample.obj 
+
+C_DEPS += \
+./TI_original_simpleBroadcast/main_TIexample.pp \
+./TI_original_simpleBroadcast/simpleBLEBroadcaster_TIexample.pp 
+
+C_DEPS__QUOTED += \
+"TI_original_simpleBroadcast\main_TIexample.pp" \
+"TI_original_simpleBroadcast\simpleBLEBroadcaster_TIexample.pp" 
+
+OBJS__QUOTED += \
+"TI_original_simpleBroadcast\main_TIexample.obj" \
+"TI_original_simpleBroadcast\simpleBLEBroadcaster_TIexample.obj" 
+
+C_SRCS__QUOTED += \
+"../TI_original_simpleBroadcast/main_TIexample.c" \
+"../TI_original_simpleBroadcast/simpleBLEBroadcaster_TIexample.c" 
+
+
