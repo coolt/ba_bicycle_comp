48ec622ebbe1f7f5f6bd0be69e27a63b485796b5
coolt <katrin@coolt.ch>
2016-04-01 15:40:54 +0200

    to ignore

--- V2_03_30/bicycle_computer/cc26xxware_2_22_00_16101/inc/hw_ioc.h
+++ V2_03_30/bicycle_computer/cc26xxware_2_22_00_16101/inc/hw_ioc.h
@@ -1,22 +1,48 @@
 /******************************************************************************
 *  Filename:       hw_ioc_h
-*  - Peripherals haben 32 Flags (DIO 0 bis DIO31).
-*    Diese werden Register offset genannt
-*  - MCU, GPIO sind beides Peripherals mit ihrer eigenen BasisAdresse
-* */
+*  Revised:        2015-09-21 14:54:10 +0200 (Mon, 21 Sep 2015)
+*  Revision:       44627
+*
+* Copyright (c) 2015, Texas Instruments Incorporated
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1) Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*
+* 2) Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+*
+* 3) Neither the name of the ORGANIZATION nor the names of its contributors may
+*    be used to endorse or promote products derived from this software without
+*    specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+******************************************************************************/
 
 #ifndef __HW_IOC_H__
 #define __HW_IOC_H__
 
 //*****************************************************************************
 //
-// This section defines the  -- register offsets -- of
-// IOC component. baek es sind die Pins/Flags einer Region/Peripherals
+// This section defines the register offsets of
+// IOC component
 //
 //*****************************************************************************
-
-// The 2 less significant bits are not used !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 // Configuration of DIO0
 #define IOC_O_IOCFG0                                                0x00000000
 
